6bb302297eb3cb9e3283a330df7e0b9da96ceeb6
Iheycuff Norambuena <ihncode@gmail.com>
2020-04-12 21:38:23 -0400

    Ahora si! Funciona bien

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-var pizza = {icon:"🍕", id: "pizza", nombre: "Pizza", tipo: "Chatarra", calorias: "8cal"};
-var manzana = {icon:"🍏", id: "manzana", nombre: "Manzana", tipo: "Fruta", calorias: "2cal"};
-var completo = {icon:"🌭", id: "completo", nombre: "Completo", tipo: "Chatarra", calorias: "7cal"};
-var tacoMexicano = {icon:"🌮", id: "tacoMexicano", nombre: "Taco Mexicano", tipo: "Chatarra", calorias: "6cal"};
-var papasFritas = {icon:"🍟", id: "papasFritas", nombre: "Papas Fritas", tipo: "Chatarra", calorias: "6cal"};
-var sandia = {icon:"🍉", id: "sandia", nombre: "Sandia", tipo: "Fruta", calorias: "2cal"};
-var hamburguesa = {icon:"🍔", id: "hamburguesa", nombre: "Hamburguesa", tipo: "Chatarra", calorias: "7cal"};
-var ji = {icon:"🍔", id: "ji", nombre: "Jiji", tipo: "Chatarra", calorias: "7cal"};
-
 var alimentos = [
-  completo, tacoMexicano, pizza, manzana,
-  hamburguesa, papasFritas, sandia
+  {icon:"🌭", id: "completo", nombre: "Completo", tipo: "Chatarra", calorias: "7cal"},
+  {icon:"🌮", id: "tacoMexicano", nombre: "Taco Mexicano", tipo: "Chatarra", calorias: "6cal"},
+  {icon:"🍕", id: "pizza", nombre: "Pizza", tipo: "Chatarra", calorias: "8cal"},
+  {icon:"🍏", id: "manzana", nombre: "Manzana", tipo: "Fruta", calorias: "2cal"},
+  {icon:"🍔", id: "hamburguesa", nombre: "Hamburguesa", tipo: "Chatarra", calorias: "7cal"},
+  {icon:"🍟", id: "papasFritas", nombre: "Papas Fritas", tipo: "Chatarra", calorias: "6cal"},
+  {icon:"🍉", id: "sandia", nombre: "Sandia", tipo: "Fruta", calorias: "2cal"}
 ]
 
 // // Esto ordena los alimentos por su tipo, primero los Chatarra y después las Frutas.
 // alimentos.sort(function (a, b) {
-//   if (a.nombre > b.nombre) {
+//   if (a.tipo > b.tipo) {
 //     return 1;
 //   }
-//   if (a.nombre < b.nombre) {
+//   if (a.tipo < b.tipo) {
 //     return -1;
 //   }
 //   // a must be equal to b
@@ -25,18 +21,10 @@ var alimentos = [
 // });
 
 
-
 function anadirElemento() {
   // var nombre = eval(this.id + ".nombre");
-
-  // SELECCIONAR DIVs DEL HTML
   var botones = document.querySelector("#botones");
   var resumen = document.querySelector("#resumen");
-  // CREAR DIVs VIRTUALES
-  // var boton = document.createElement("div");
-  // var dato = document.createElement("div");
-  // CONTADOR
-  // var miContador = document.querySelector("#contador-" + this.id).innerHTML + 1;
 
   alimentos.forEach(function(e) {
     var boton = document.createElement("div");
@@ -52,20 +40,23 @@ function anadirElemento() {
   alimentos.forEach(function(e) {
     var dato = document.createElement("div");
     dato.innerHTML =
-      '<div class="dato">'
+      '<div class="dato" id="dato-' + e.id + '" style="display: none">'
         + '<span id="contador-' + e.id + '">' + "0" + '</span>' + e.nombre
       + '</div>';
     resumen.appendChild(dato);
   });
-
-  // miContador = document.querySelector("#contador-" + this.id).innerHTML + 1;
-
 }
 
 anadirElemento();
 
 function contarElemento() {
   var contadorDelElemento = document.querySelector("#contador-" + this.id);
+
+  if (contadorDelElemento.innerHTML == "0") {
+    var dato = document.querySelector("#dato-" + this.id);
+    dato.setAttribute("style", "display: block");
+  }
+
   contadorDelElemento.innerHTML = parseInt(contadorDelElemento.innerHTML) + 1;
 }
 
